31d3bb81276385eeb1e3c4f993d7d105158d98e6
javito0003 <javbermu@ucm.es>
2015-11-14 16:39:49 +0100

    Added test_persistence

---
+++ src/test/test_persistence.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "persistence.h"
+
+int main(){
+  MYSQL* bd;
+  bd = mysql_init(NULL);
+  init_bd(bd,"root","calasancio3","PSD");
+  
+  add_user(bd,10,"pepe","information asdasdd asdasdaa sdaaasdas");
+  //add_user(bd,2,"antonio","information");
+  //del_user(bd,"pepe");
+ //printf("%d\n",exist_user(bd,"pe12312pe2"));
+  //accept_friend(bd,1,2);
+  //send_request(bd,1,2);
+  //accept_friend(bd,1,2);
+  //refuse_request(bd,1,2);
+  //del_friends(bd,1,2); 
+   //get_id_user(bd,"antonio");
+  //add_chat(bd,2,2,"informarcion");
+  //del_chat(bd,1);
+  //add_user_chat(bd,4,1);
+  del_user_chat(bd,2,1);
+return 0;
+}
